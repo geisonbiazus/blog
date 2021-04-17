ff9f52e42318f34b5c2cdbc9f5d1ad70e7b089f6
Geison Biazus <geisonbiazus@gmail.com>
2021-04-17 14:21:01 +0200

    Implement GetAllPosts on PotRepo

--- internal/adapters/postrepo/filesystem/post_repo.go
+++ internal/adapters/postrepo/filesystem/post_repo.go
@@ -1,7 +1,11 @@
 package filesystem
 
 import (
+	"io/fs"
 	"io/ioutil"
+	"os"
+	"sort"
+	"strings"
 
 	"github.com/geisonbiazus/blog/internal/core/posts"
 )
@@ -25,4 +29,38 @@ func (r *PostRepo) GetPostByPath(path string) (posts.Post, error) {
 	post.Path = path
 
 	return post, err
+}
+
+func (r *PostRepo) GetAllPosts() ([]posts.Post, error) {
+	postList := []posts.Post{}
+	entries, err := os.ReadDir(r.BasePath)
+
+	if err != nil {
+		return postList, err
+	}
+
+	for _, entry := range entries {
+		postList, err = r.maybeLoadPostFromFile(postList, entry)
+	}
+
+	sort.Slice(postList, func(i, j int) bool {
+		return postList[i].Time.After(postList[j].Time)
+	})
+
+	return postList, err
+}
+
+func (r *PostRepo) maybeLoadPostFromFile(postList []posts.Post, entry fs.DirEntry) ([]posts.Post, error) {
+	if !strings.HasSuffix(entry.Name(), ".md") {
+		return postList, nil
+	}
+
+	fileName := strings.TrimSuffix(entry.Name(), ".md")
+	post, err := r.GetPostByPath(fileName)
+
+	if err != nil {
+		return postList, err
+	}
+
+	return append(postList, post), nil
 }
--- internal/adapters/postrepo/filesystem/post_repo_test.go
+++ internal/adapters/postrepo/filesystem/post_repo_test.go
@@ -21,30 +21,78 @@ func TestPostRepo(t *testing.T) {
 		}
 	}
 
-	t.Run("It returns error when post is not found", func(t *testing.T) {
-		f := setup()
+	t.Run("GetPostByPath", func(t *testing.T) {
+		t.Run("It returns error when post is not found", func(t *testing.T) {
+			f := setup()
 
-		post, err := f.repo.GetPostByPath("wrong_path")
+			post, err := f.repo.GetPostByPath("wrong_path")
 
-		assert.DeepEqual(t, posts.Post{}, post)
-		assert.Equal(t, posts.ErrPostNotFound, err)
+			assert.DeepEqual(t, posts.Post{}, post)
+			assert.Equal(t, posts.ErrPostNotFound, err)
+		})
+
+		t.Run("It returns parsed post when the file exists", func(t *testing.T) {
+			f := setup()
+
+			post, err := f.repo.GetPostByPath("test-post")
+
+			assert.Nil(t, err)
+			assert.DeepEqual(t, testPost1, post)
+		})
 	})
 
-	t.Run("It returns parsed post when the file exists", func(t *testing.T) {
-		f := setup()
+	t.Run("GetAllPosts()", func(t *testing.T) {
+		t.Run("Given a path with no post files, it returns empty", func(t *testing.T) {
+			repo := filesystem.NewPostRepo(".")
+			postList, err := repo.GetAllPosts()
 
-		post, err := f.repo.GetPostByPath("test-post")
+			assert.DeepEqual(t, []posts.Post{}, postList)
+			assert.Nil(t, err)
+		})
 
-		assert.Nil(t, err)
-		assert.DeepEqual(t, posts.Post{
-			Title:  "Test Post",
-			Author: "Geison Biazus",
-			Path:   "test-post",
-			Time:   toTime("2021-04-05T18:47:00Z"),
-			Content: "" +
-				"## Subtitle\n" +
-				"\n" +
-				"Content\n",
-		}, post)
+		t.Run("Given a non-existent path, it returns error", func(t *testing.T) {
+			repo := filesystem.NewPostRepo("./invalid-path")
+			postList, err := repo.GetAllPosts()
+
+			assert.DeepEqual(t, []posts.Post{}, postList)
+			assert.NotNil(t, err)
+		})
+
+		t.Run("Given a path with post files, it returns all posts sorted by descending date", func(t *testing.T) {
+			f := setup()
+			expectedPosts := []posts.Post{testPost1, testPost2, testPost3}
+
+			actualPosts, err := f.repo.GetAllPosts()
+
+			assert.DeepEqual(t, expectedPosts, actualPosts)
+			assert.Nil(t, err)
+		})
 	})
+}
+
+var testPost1 = posts.Post{
+	Title:  "Test Post",
+	Author: "Geison Biazus",
+	Path:   "test-post",
+	Time:   toTime("2021-04-05T18:47:00Z"),
+	Content: "" +
+		"## Subtitle\n" +
+		"\n" +
+		"Content\n",
+}
+
+var testPost2 = posts.Post{
+	Title:   "Test Post 3",
+	Author:  "Geison Biazus",
+	Path:    "test-post-3",
+	Time:    toTime("2021-04-05T18:40:00Z"),
+	Content: "",
+}
+
+var testPost3 = posts.Post{
+	Title:   "Test Post 2",
+	Author:  "Geison Biazus",
+	Path:    "test-post-2",
+	Time:    toTime("2021-04-04T14:33:00Z"),
+	Content: "",
 }
