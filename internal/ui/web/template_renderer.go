da4b71305af5f8418a9aa3d5fee40c9e1b190879
Geison Biazus <geisonbiazus@gmail.com>
2021-04-21 08:01:13 +0200

    Add header layout

--- internal/ui/web/template_renderer.go
+++ internal/ui/web/template_renderer.go
@@ -8,16 +8,20 @@ import (
 )
 
 type TemplateRenderer struct {
+	path string
 	tmpl *template.Template
 }
 
 func NewTemplateRenderer(basePath string) (*TemplateRenderer, error) {
 	path := fmt.Sprintf("%s/*", basePath)
 	tmpl, err := template.ParseGlob(path)
-	return &TemplateRenderer{tmpl: tmpl}, err
+	return &TemplateRenderer{tmpl: tmpl, path: path}, err
 }
 
 func (r *TemplateRenderer) Render(writer io.Writer, templateName string, data interface{}) {
+	tmpl, _ := template.ParseGlob(r.path)
+	r.tmpl = tmpl
+
 	buf := bytes.NewBuffer([]byte{})
 	r.tmpl.ExecuteTemplate(buf, templateName, data)
 	r.tmpl.ExecuteTemplate(writer, "layout.html", template.HTML(buf.String()))
