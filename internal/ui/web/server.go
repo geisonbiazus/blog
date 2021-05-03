4c05d2ec29d50948f16a77519880b5c8dcee5815
Geison Biazus <geisonbiazus@gmail.com>
2021-05-03 13:19:25 +0200

    Add logger to server

--- internal/ui/web/server.go
+++ internal/ui/web/server.go
@@ -2,22 +2,26 @@ package web
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
 type Server struct {
 	Port   int
 	Router http.Handler
+	Logger *log.Logger
 }
 
-func NewServer(port int, router http.Handler) *Server {
+func NewServer(port int, router http.Handler, logger *log.Logger) *Server {
 	return &Server{
 		Port:   port,
 		Router: router,
+		Logger: logger,
 	}
 }
 
 func (s *Server) Start() error {
 	addr := fmt.Sprintf(":%v", s.Port)
+	s.Logger.Printf("Starting server on port %v", s.Port)
 	return http.ListenAndServe(addr, s.Router)
 }
