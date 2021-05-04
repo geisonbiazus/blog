d72a8a782d6babf9d9684c6daca15442401c3249
Geison Biazus <geisonbiazus@gmail.com>
2021-05-04 13:33:26 +0200

    Add syntax highlighting

--- internal/adapters/renderer/goldmark/renderer.go
+++ internal/adapters/renderer/goldmark/renderer.go
@@ -3,7 +3,9 @@ package goldmark
 import (
 	"bytes"
 
+	"github.com/alecthomas/chroma/formatters/html"
 	"github.com/yuin/goldmark"
+	highlighting "github.com/yuin/goldmark-highlighting"
 )
 
 type GoldmarkRenderer struct{}
@@ -14,7 +16,19 @@ func NewGoldmarkRenderer() *GoldmarkRenderer {
 
 func (r *GoldmarkRenderer) Render(content string) (string, error) {
 	var buf bytes.Buffer
-	err := goldmark.Convert([]byte(content), &buf)
+
+	markdown := goldmark.New(
+		goldmark.WithExtensions(
+			highlighting.NewHighlighting(
+				highlighting.WithStyle("monokai"),
+				highlighting.WithFormatOptions(
+					html.TabWidth(2),
+				),
+			),
+		),
+	)
+
+	err := markdown.Convert([]byte(content), &buf)
 
 	if err != nil {
 		return "", err
