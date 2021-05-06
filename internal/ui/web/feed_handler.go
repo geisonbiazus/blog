8b05b757bb7822e9d099c34f9af7e5330d984436
Geison Biazus <geisonbiazus@gmail.com>
2021-05-06 09:32:36 +0200

    Refactor FeedHandler

--- internal/ui/web/feed_handler.go
+++ internal/ui/web/feed_handler.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/geisonbiazus/blog/internal/core/blog"
 	"github.com/gorilla/feeds"
 )
 
@@ -21,26 +22,40 @@ func NewFeedHandler(usecase ListPostUseCase, templateRenderer *TemplateRenderer)
 
 func (h *FeedHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	posts, _ := h.usecase.Run()
+	feed := h.buildFeed(posts)
 
-	feed := &feeds.Feed{
+	w.Header().Add("Content-Type", "application/atom+xml")
+	w.WriteHeader(http.StatusOK)
+	feed.WriteAtom(w)
+}
+
+func (h *FeedHandler) buildFeed(posts []blog.Post) *feeds.Feed {
+	return &feeds.Feed{
 		Title:       "Geison Biazus Blog",
 		Link:        &feeds.Link{Href: "https://blog.geisonbiazus.com"},
 		Description: "My personal blog about software development.",
 		Author:      &feeds.Author{Name: "Geison Biazus", Email: "geisonbiazus@gmail.com"},
 		Created:     posts[0].Time,
+		Items:       h.buildFeedItems(posts),
 	}
+}
+
+func (h *FeedHandler) buildFeedItems(posts []blog.Post) []*feeds.Item {
+	items := []*feeds.Item{}
 
 	for _, post := range posts {
-		feed.Items = append(feed.Items, &feeds.Item{
-			Title:   post.Title,
-			Link:    &feeds.Link{Href: fmt.Sprintf("https://blog.geisonbiazus.com/%s", post.Path)},
-			Content: post.Content,
-			Author:  &feeds.Author{Name: post.Author},
-			Created: post.Time,
-		})
+		items = append(items, h.buildFeedItem(post))
 	}
 
-	w.Header().Add("Content-Type", "application/atom+xml")
-	w.WriteHeader(http.StatusOK)
-	feed.WriteAtom(w)
+	return items
+}
+
+func (h *FeedHandler) buildFeedItem(post blog.Post) *feeds.Item {
+	return &feeds.Item{
+		Title:   post.Title,
+		Link:    &feeds.Link{Href: fmt.Sprintf("https://blog.geisonbiazus.com/%s", post.Path)},
+		Content: post.Content,
+		Author:  &feeds.Author{Name: post.Author},
+		Created: post.Time,
+	}
 }
