5d5c3c89fd05b5629f4e4ae12c735ebd67cbd470
Geison Biazus <geisonbiazus@gmail.com>
2021-05-07 09:09:36 +0200

    FeedHandler use dynamic base path and return empty feed

--- internal/ui/web/feed_handler.go
+++ internal/ui/web/feed_handler.go
@@ -3,6 +3,7 @@ package web
 import (
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/geisonbiazus/blog/internal/core/blog"
 	"github.com/gorilla/feeds"
@@ -11,12 +12,14 @@ import (
 type FeedHandler struct {
 	usecase  ListPostUseCase
 	template *TemplateRenderer
+	baseURL  string
 }
 
-func NewFeedHandler(usecase ListPostUseCase, templateRenderer *TemplateRenderer) *FeedHandler {
+func NewFeedHandler(usecase ListPostUseCase, templateRenderer *TemplateRenderer, baseURL string) *FeedHandler {
 	return &FeedHandler{
 		usecase:  usecase,
 		template: templateRenderer,
+		baseURL:  baseURL,
 	}
 }
 
@@ -32,12 +35,21 @@ func (h *FeedHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 func (h *FeedHandler) buildFeed(posts []blog.Post) *feeds.Feed {
 	return &feeds.Feed{
 		Title:       "Geison Biazus Blog",
-		Link:        &feeds.Link{Href: "https://blog.geisonbiazus.com"},
+		Link:        &feeds.Link{Href: h.baseURL},
 		Description: "My personal blog about software development.",
 		Author:      &feeds.Author{Name: "Geison Biazus", Email: "geisonbiazus@gmail.com"},
-		Created:     posts[0].Time,
+		Created:     h.resolveUpdatedTime(posts),
 		Items:       h.buildFeedItems(posts),
 	}
+}
+
+func (h *FeedHandler) resolveUpdatedTime(posts []blog.Post) time.Time {
+	if len(posts) == 0 {
+		defaultTime, _ := time.Parse(time.RFC3339, "2021-04-01T12:00:00Z")
+		return defaultTime
+	}
+
+	return posts[0].Time
 }
 
 func (h *FeedHandler) buildFeedItems(posts []blog.Post) []*feeds.Item {
@@ -53,7 +65,7 @@ func (h *FeedHandler) buildFeedItems(posts []blog.Post) []*feeds.Item {
 func (h *FeedHandler) buildFeedItem(post blog.Post) *feeds.Item {
 	return &feeds.Item{
 		Title:   post.Title,
-		Link:    &feeds.Link{Href: fmt.Sprintf("https://blog.geisonbiazus.com/%s", post.Path)},
+		Link:    &feeds.Link{Href: fmt.Sprintf("%s/%s", h.baseURL, post.Path)},
 		Content: post.Content,
 		Author:  &feeds.Author{Name: post.Author},
 		Created: post.Time,
--- internal/ui/web/feed_handler_test.go
+++ internal/ui/web/feed_handler_test.go
@@ -2,7 +2,7 @@ package web_test
 
 import (
 	"net/http"
-	"strings"
+	"regexp"
 	"testing"
 
 	"github.com/geisonbiazus/blog/internal/core/blog"
@@ -20,7 +20,8 @@ func TestFeedPostsHandler(t *testing.T) {
 	setup := func() *feedHandlerFixture {
 		usecase := &listPostUseCaseSpy{}
 		templateRenderer := newTestTemplateRenderer()
-		handler := web.NewFeedHandler(usecase, templateRenderer)
+		baseURL := "http://example.com"
+		handler := web.NewFeedHandler(usecase, templateRenderer, baseURL)
 
 		return &feedHandlerFixture{
 			usecase: usecase,
@@ -38,23 +39,40 @@ func TestFeedPostsHandler(t *testing.T) {
 
 		assert.Equal(t, http.StatusOK, res.StatusCode)
 		assert.Equal(t, "application/atom+xml", res.Header.Get("Content-Type"))
-		assert.Equal(t, cleanString(expectedFeed), cleanString(body))
+		assertFeedEqual(t, expectedFeed, body)
+	})
+
+	t.Run("Given no post exists it returns the empty feed", func(t *testing.T) {
+		f := setup()
+
+		f.usecase.ReturnPosts = []blog.Post{}
+
+		res := doGetRequest(f.handler, "/feed.atom")
+		body := testhelper.ReadResponseBody(res)
+
+		assert.Equal(t, http.StatusOK, res.StatusCode)
+		assert.Equal(t, "application/atom+xml", res.Header.Get("Content-Type"))
+		assertFeedEqual(t, expectedEmptyFeed, body)
 	})
 }
 
-func cleanString(source string) string {
-	source = strings.ReplaceAll(source, " ", "")
-	source = strings.ReplaceAll(source, "\n", "")
-	source = strings.ReplaceAll(source, "\t", "")
-	return source
+func assertFeedEqual(t *testing.T, expected, actual string) {
+	t.Helper()
+	assert.Equal(t, removeWhiteSpaces(expected), removeWhiteSpaces(actual))
+}
+
+var reWhiteSpace = regexp.MustCompile(`\s`)
+
+func removeWhiteSpaces(source string) string {
+	return reWhiteSpace.ReplaceAllString(source, "")
 }
 
 var expectedFeed = `<?xml version="1.0" encoding="UTF-8"?><feed xmlns="http://www.w3.org/2005/Atom">
 	<title>Geison Biazus Blog</title>
-	<id>https://blog.geisonbiazus.com</id>
+	<id>http://example.com</id>
 	<updated>2021-04-04T14:33:00Z</updated>
 	<subtitle>My personal blog about software development.</subtitle>
-	<link href="https://blog.geisonbiazus.com"></link>
+	<link href="http://example.com"></link>
 	<author>
 		<name>Geison Biazus</name>
 		<email>geisonbiazus@gmail.com</email>
@@ -62,9 +80,9 @@ var expectedFeed = `<?xml version="1.0" encoding="UTF-8"?><feed xmlns="http://ww
 	<entry>
 		<title>Test Post 2</title>
 		<updated>2021-04-04T14:33:00Z</updated>
-		<id>tag:blog.geisonbiazus.com,2021-04-04:/test-post-2</id>
+		<id>tag:example.com,2021-04-04:/test-post-2</id>
 		<content type="html">Content for post 2</content>
-		<link href="https://blog.geisonbiazus.com/test-post-2" rel="alternate"></link>
+		<link href="http://example.com/test-post-2" rel="alternate"></link>
 		<summary type="html"></summary>
 		<author>
 			<name>Geison Biazus</name>
@@ -73,12 +91,24 @@ var expectedFeed = `<?xml version="1.0" encoding="UTF-8"?><feed xmlns="http://ww
 	<entry>
 		<title>Test Post 1</title>
 		<updated>2021-04-05T18:47:00Z</updated>
-		<id>tag:blog.geisonbiazus.com,2021-04-05:/test-post-1</id>
+		<id>tag:example.com,2021-04-05:/test-post-1</id>
 		<content type="html">Content for post 1</content>
-		<link href="https://blog.geisonbiazus.com/test-post-1" rel="alternate"></link>
+		<link href="http://example.com/test-post-1" rel="alternate"></link>
 		<summary type="html"></summary>
 		<author>
 			<name>Geison Biazus</name>
 		</author>
 	</entry>
+</feed>`
+
+var expectedEmptyFeed = `<?xml version="1.0" encoding="UTF-8"?><feed xmlns="http://www.w3.org/2005/Atom">
+	<title>Geison Biazus Blog</title>
+	<id>http://example.com</id>
+	<updated>2021-04-01T12:00:00Z</updated>
+	<subtitle>My personal blog about software development.</subtitle>
+	<link href="http://example.com"></link>
+	<author>
+		<name>Geison Biazus</name>
+		<email>geisonbiazus@gmail.com</email>
+	</author>
 </feed>`
