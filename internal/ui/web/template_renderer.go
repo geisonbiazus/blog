e890cf31fe3463fa250be6fc7416bb6d825bcf50
Geison Biazus <geisonbiazus@gmail.com>
2021-05-11 09:07:00 +0200

    Cache template files

--- internal/ui/web/template_renderer.go
+++ internal/ui/web/template_renderer.go
@@ -7,21 +7,45 @@ import (
 )
 
 type TemplateRenderer struct {
-	basePath string
-	tmpl     *template.Template
+	basePath        string
+	tmpl            *template.Template
+	cachedTemplates map[string]*template.Template
 }
 
 func NewTemplateRenderer(basePath string) (*TemplateRenderer, error) {
 	tmpl, err := template.ParseFiles(filepath.Join(basePath, "layout.html"))
-	return &TemplateRenderer{tmpl: tmpl, basePath: basePath}, err
+	return &TemplateRenderer{
+		tmpl:            tmpl,
+		basePath:        basePath,
+		cachedTemplates: map[string]*template.Template{},
+	}, err
 }
 
 func (r *TemplateRenderer) Render(writer io.Writer, templateName string, data interface{}) {
-	tmpl, err := template.ParseFiles(filepath.Join(r.basePath, "layout.html"), filepath.Join(r.basePath, templateName))
+	tmpl := r.resolveTemplate(templateName)
+	tmpl.Execute(writer, data)
+}
+
+func (r *TemplateRenderer) resolveTemplate(name string) *template.Template {
+	tmpl, ok := r.cachedTemplates[name]
+
+	if !ok {
+		tmpl = r.parseTemplate(name)
+		r.cachedTemplates[name] = tmpl
+	}
+
+	return tmpl
+}
+
+func (r *TemplateRenderer) parseTemplate(name string) *template.Template {
+	tmpl, err := template.ParseFiles(
+		filepath.Join(r.basePath, "layout.html"),
+		filepath.Join(r.basePath, name),
+	)
 
 	if err != nil {
 		panic(err)
 	}
 
-	tmpl.Execute(writer, data)
+	return tmpl
 }
