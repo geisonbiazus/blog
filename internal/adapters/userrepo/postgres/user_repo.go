6bf5e5eeeab2f800fc21601dd8b138c8da35f09e
Geison Biazus <geisonbiazus@gmail.com>
2021-11-12 09:45:37 +0100

    Get tx from context

--- internal/adapters/userrepo/postgres/user_repo.go
+++ internal/adapters/userrepo/postgres/user_repo.go
@@ -15,16 +15,36 @@ type Querier interface {
 	QueryRowContext(ctx context.Context, query string, args ...interface{}) *sql.Row
 }
 
+type TxKeyType string
+
+var TxKey = TxKeyType("tx")
+
 type UserRepo struct {
-	querier Querier
+	db *sql.DB
 }
 
-func NewUserRepo(querier Querier) *UserRepo {
-	return &UserRepo{querier: querier}
+func NewUserRepo(db *sql.DB) *UserRepo {
+	return &UserRepo{db: db}
+}
+
+func (r *UserRepo) querier(ctx context.Context) Querier {
+	tx := ctx.Value(TxKey)
+
+	if tx == nil {
+		return r.db
+	}
+
+	if tx, ok := tx.(*sql.Tx); ok {
+		return tx
+	}
+
+	return r.db
 }
 
 func (r *UserRepo) CreateUser(ctx context.Context, user auth.User) error {
-	result, err := r.querier.ExecContext(ctx,
+	q := r.querier(ctx)
+
+	result, err := q.ExecContext(ctx,
 		"INSERT INTO users (id, name, email, provider_user_id, avatar_url) VALUES ($1, $2, $3, $4, $5)",
 		user.ID, user.Name, user.Email, user.ProviderUserID, user.AvatarURL,
 	)
@@ -41,7 +61,9 @@ func (r *UserRepo) CreateUser(ctx context.Context, user auth.User) error {
 }
 
 func (r *UserRepo) FindById(ctx context.Context, id string) (auth.User, error) {
-	row := r.querier.QueryRowContext(ctx, "SELECT id, name, email, provider_user_id, avatar_url FROM users where id = $1", id)
+	q := r.querier(ctx)
+
+	row := q.QueryRowContext(ctx, "SELECT id, name, email, provider_user_id, avatar_url FROM users where id = $1", id)
 
 	user := auth.User{}
 
--- internal/adapters/userrepo/postgres/user_repo_test.go
+++ internal/adapters/userrepo/postgres/user_repo_test.go
@@ -11,32 +11,39 @@ import (
 	"github.com/geisonbiazus/blog/pkg/assert"
 )
 
+func transactional(ctx context.Context, db *sql.DB, cb func(ctx context.Context)) {
+	tx, _ := db.BeginTx(ctx, nil)
+	ctx = context.WithValue(ctx, postgres.TxKey, tx)
+	cb(ctx)
+	tx.Rollback()
+}
+
 func TestUserRepo(t *testing.T) {
 	t.Run("CreateUser", func(t *testing.T) {
 		ctx := context.Background()
 		db, _ := sql.Open("pgx", "postgres://postgres:postgres@localhost:5433/blog_test?sslmode=disable")
-		tx, _ := db.BeginTx(ctx, nil)
-		defer tx.Rollback()
 		defer db.Close()
-
-		repo := postgres.NewUserRepo(tx)
+		repo := postgres.NewUserRepo(db)
 		uuidGen := uuid.NewGenerator()
 
-		user := auth.User{
-			ID:             uuidGen.Generate(),
-			Name:           "Name",
-			Email:          "user@example.com",
-			ProviderUserID: "provider_user_id",
-			AvatarURL:      "http://example.com/avatar",
-		}
+		transactional(ctx, db, func(ctx context.Context) {
+			user := auth.User{
+				ID:             uuidGen.Generate(),
+				Name:           "Name",
+				Email:          "user@example.com",
+				ProviderUserID: "provider_user_id",
+				AvatarURL:      "http://example.com/avatar",
+			}
 
-		err := repo.CreateUser(ctx, user)
+			err := repo.CreateUser(ctx, user)
 
-		assert.Nil(t, err)
+			assert.Nil(t, err)
 
-		createdUser, err := repo.FindById(ctx, user.ID)
+			createdUser, err := repo.FindById(ctx, user.ID)
 
-		assert.Nil(t, err)
-		assert.Equal(t, createdUser, user)
+			assert.Nil(t, err)
+			assert.Equal(t, createdUser, user)
+		})
+
 	})
 }
