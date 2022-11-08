8a2c8472063111faa7a4e32c6befcfc4354ebe29
Geison Biazus <geisonbiazus@gmail.com>
2022-11-08 08:59:58 -0300

    Refactor CommentRepo test

--- internal/adapters/commentrepo/postgres/comment_repo_test.go
+++ internal/adapters/commentrepo/postgres/comment_repo_test.go
@@ -3,7 +3,6 @@ package postgres_test
 import (
 	"context"
 	"database/sql"
-	"fmt"
 	"testing"
 	"time"
 
@@ -15,48 +14,66 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+type commentRepoFixture struct {
+	repo      *postgres.CommentRepo
+	uuidGen   *uuid.Generator
+	author    *discussion.Author
+	subjectID string
+	comment1  *discussion.Comment
+	comment2  *discussion.Comment
+}
+
 func TestCommentRepo(t *testing.T) {
+	setup := func(db *sql.DB) *commentRepoFixture {
+		uuidGen := uuid.NewGenerator()
+
+		author := NewAuthor(discussion.Author{ID: uuidGen.Generate()})
+		subjectID := "SUBJECT_ID"
+
+		comment1 := NewComment(discussion.Comment{
+			ID:        uuidGen.Generate(),
+			SubjectID: subjectID,
+			AuthorID:  author.ID,
+			Author:    author,
+			CreatedAt: time.Date(2022, time.October, 4, 9, 0, 0, 0, time.UTC),
+		})
+
+		comment2 := NewComment(discussion.Comment{
+			ID:        uuidGen.Generate(),
+			SubjectID: subjectID,
+			AuthorID:  author.ID,
+			Author:    author,
+			CreatedAt: time.Date(2022, time.October, 4, 10, 0, 0, 0, time.UTC),
+		})
+
+		repo := postgres.NewCommentRepo(db)
+
+		return &commentRepoFixture{
+			repo:      repo,
+			uuidGen:   uuidGen,
+			author:    author,
+			subjectID: subjectID,
+			comment1:  comment1,
+			comment2:  comment2,
+		}
+	}
+
 	t.Run("GetCommentsAndRepliesRecursively", func(t *testing.T) {
 		t.Run("It fetches the comments by subjectID", func(t *testing.T) {
 			dbrepo.Test(func(ctx context.Context, db *sql.DB) {
-				uuidGen := uuid.NewGenerator()
+				f := setup(db)
 
-				author := NewAuthor(discussion.Author{ID: uuidGen.Generate()})
-				subjectID := "SUBJECT_ID"
+				assert.Nil(t, f.repo.SaveAuthor(ctx, f.author))
+				assert.Nil(t, f.repo.SaveComment(ctx, f.comment1))
+				assert.Nil(t, f.repo.SaveComment(ctx, f.comment2))
 
-				comment1 := NewComment(discussion.Comment{
-					ID:        uuidGen.Generate(),
-					SubjectID: subjectID,
-					AuthorID:  author.ID,
-					Author:    author,
-					CreatedAt: time.Date(2022, time.October, 4, 9, 0, 0, 0, time.UTC),
-				})
-
-				comment2 := NewComment(discussion.Comment{
-					ID:        uuidGen.Generate(),
-					SubjectID: subjectID,
-					AuthorID:  author.ID,
-					Author:    author,
-					CreatedAt: time.Date(2022, time.October, 4, 10, 0, 0, 0, time.UTC),
-				})
-
-				repo := postgres.NewCommentRepo(db)
-
-				assert.Nil(t, repo.SaveAuthor(ctx, author))
-				assert.Nil(t, repo.SaveComment(ctx, comment1))
-				assert.Nil(t, repo.SaveComment(ctx, comment2))
-
-				comments, err := repo.GetCommentsAndRepliesRecursively(ctx, subjectID)
+				comments, err := f.repo.GetCommentsAndRepliesRecursively(ctx, f.subjectID)
 
 				assert.Nil(t, err)
 				assert.Equal(t, []*discussion.Comment{
-					comment1,
-					comment2,
+					f.comment1,
+					f.comment2,
 				}, comments)
-
-				for _, c := range comments {
-					fmt.Println(c)
-				}
 			})
 		})
 	})
