64238a0eab51029cc09b80ffcdebc764dbedcfe7
Geison Biazus <geisonbiazus@gmail.com>
2023-03-27 08:47:13 -0300

    Add InMemory PubSub

---
+++ internal/adapters/pubsub/memory/pubsub.go
@@ -0,0 +1,39 @@
+package memory
+
+import (
+	"github.com/geisonbiazus/blog/internal/core/shared"
+)
+
+type PubSub struct {
+	SubscriberBuffer int
+	subscribers      map[string][]chan shared.Event
+}
+
+func NewPubSub() *PubSub {
+	return &PubSub{
+		SubscriberBuffer: 10,
+		subscribers:      map[string][]chan shared.Event{},
+	}
+}
+
+func (p *PubSub) Publish(event shared.Event) error {
+	for _, subscriber := range p.subscribers[event.Type] {
+		subscriber <- event
+	}
+
+	return nil
+}
+
+func (p *PubSub) Subscribe(eventType string) chan shared.Event {
+	channel := make(chan shared.Event, p.SubscriberBuffer)
+	p.ensureSubscribersFor(eventType)
+	p.subscribers[eventType] = append(p.subscribers[eventType], channel)
+
+	return channel
+}
+
+func (p *PubSub) ensureSubscribersFor(eventType string) {
+	if _, ok := p.subscribers[eventType]; !ok {
+		p.subscribers[eventType] = []chan shared.Event{}
+	}
+}
--- internal/core/discussion/ports.go
+++ internal/core/discussion/ports.go
@@ -4,6 +4,6 @@ import "context"
 
 type CommentRepo interface {
 	SaveAuthor(ctx context.Context, author *Author) error
-	GetAuthorByID(ctx context.Context, ID string) (*Author, error)
+	GetAuthorByID(ctx context.Context, id string) (*Author, error)
 	GetCommentsAndRepliesRecursively(ctx context.Context, subjectID string) ([]*Comment, error)
 }
